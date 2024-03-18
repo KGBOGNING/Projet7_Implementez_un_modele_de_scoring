611d2677983f5ea2660af3372330e7bb25ee780c
kgbogningtsakem <kgbogning@gmail.com>
2024-03-18 17:02:42 +0100

    ajout des tests dans le Yamml

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 import pytest
 import os
-from config import config
+from ..config import config
 
 # config.update({'PROJECT_DIR': os.path.dirname(__file__) + '/'})
 config.update({'TESTING': True})
 
-from .backend import create_app
+from ..backend import create_app
 
 
 @pytest.fixture
