93a40eb120437c67c06300656fd8f0f5d22e98b7
kgbogningtsakem <kgbogning@gmail.com>
2024-03-18 19:31:17 +0100

    Mise à jour de la fonction test cordonnées absolue

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
 import pytest
 import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '', '')))
+
 from config import config
 
 # config.update({'PROJECT_DIR': os.path.dirname(__file__) + '/'})
